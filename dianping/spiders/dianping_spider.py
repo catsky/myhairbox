de6e6644c5738905c21a77c3672eec4d3241628f
Ken <zhdhui@gmail.com>
2012-01-07 23:56:28 +0800

    first workable version: ready to crawl the dianping website, it records "title","address","price","comment","tag" etc. enjoy.

--- dianping/spiders/dianping_spider.py
+++ dianping/spiders/dianping_spider.py
@@ -1,26 +1,45 @@
-from scrapy.spider import BaseSpider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.spiders import Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.http import Request
 from scrapy.selector import HtmlXPathSelector
 
 from dianping.items import DianpingItem
 
-class DmozSpider(BaseSpider):
+class DmozSpider(CrawlSpider):
     name="dianping"
     allowed_domains=["dianping.com"]
+    first_time = True
     start_urls=[
-    "http://www.dianping.com/search/category/1/50/g157",
+    "http://www.dianping.com/search/category/1/50/g157p1",
          ]
-
-    def parse(self, response):
+    #'http://www.dianping.com/search/category/1/50/g157p2'
+    rules = (   
+        Rule(SgmlLinkExtractor(allow="/search/category/1/50/g157p\d*$"),
+            'parse_info',
+            follow=True,
+        ),
+    )
+    def parse_info(self, response):
         #filename =response.url.split("/")[-2]
         #open(filename, 'wb').write(response.body)
         hxs = HtmlXPathSelector(response)
-        sites = hxs.select("//li[@class='shopname']/a[@class='BL']")
-        //li[@class='address']/strong
-        //li[@class='address']d
+        sites = hxs.select("//dd[child::ul[@class='remark']]")
+
         items = []
         for site in sites:
             item = DianpingItem()
-            item['name'] = site.select('text()').extract()
-            items.append(item)
-        return items
+            item['name'] = site.select("descendant::li[@class='shopname']/a/text()").extract()
+            item['address'] = site.select("descendant::li[@class='address']/descendant::text()").extract()
+ #           item['telphone'] = site.select("descendant::li[@class='shopname']/a/text()").extract()
+            item['tag'] = site.select("descendant::li[@class='tags']/descendant::text()").extract()
+            item['avgPrice'] = site.select("descendant::strong[@class='average']/text()").extract()
+            item['commentAff'] = site.select("descendant::li[@class='grade']/span[@class='score1']/text()").extract()
+            item['commentEnv'] = site.select("descendant::li[@class='grade']/span[@class='score2']/text()").extract()
+            item['commentServ'] = site.select("descendant::li[@class='grade']/span[@class='score3']/text()").extract()
 
+            items.append(item)
+ #       if  first_time == True:
+ #           first_time = False
+ #           yield Request(url="http://www.dianping.com/search/category/1/50/g15", callback=self.parse)
+        return items
---
+++ myjson_trans_unicode.py
@@ -0,0 +1,10 @@
+f=file("items.json")
+out=file("items_out.json","w+")
+results = []
+for item in f:
+    str=item.decode("unicode_escape")
+    results.append(str)
+
+print "\n".join(results)
+#out.writelines("\n".join(results))
+
